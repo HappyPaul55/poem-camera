732dd0f61f2bff6e992bd0fbc10a3e902ab32330
Paul Hutchinson <paul@lexiphanic.co.uk>
2024-12-21 13:30:17 +0000

    Branding!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,13 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0" />
+        <meta name="apple-mobile-web-app-title" content="PoemCam" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="PoemCam" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body
         className="antialiased"
